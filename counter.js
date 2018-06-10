7f1a335db1fcdaf6ee506867c2c45bfd52a54ce9
Oron Sharabi <oron@bigpanda.io>
2018-06-10 14:40:11 +0300

    added function count to twenty

--- counter.js
+++ counter.js
@@ -12,8 +12,19 @@
 	}
     }
 
+    function countToTwenty() {
+	for(var i=0; i < 20; i++) {
+	    (function(index) {
+		setTimeout(function() {
+		    console.log(index+1);
+		}, index * 1000);
+	    }(i));
+	}
+    }
 
-    this.countToTen = countToTen;
+
+    this.countToTen    = countToTen;
+    this.countToTwenty = countToTwenty;    
 
 }());
 
