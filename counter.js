e2d6d848adcca959f212d66cf8fa79b6c903bc06
Oron Sharabi <oron@bigpanda.io>
2018-06-10 14:53:11 +0300

    refactor: generic counter function
    create a generic counter function and refactor countToTen

--- counter.js
+++ counter.js
@@ -3,26 +3,25 @@
 (function() {
     
     function countToTen() {
-	for(var i=0; i < 10; i++) {
-	    (function(index) {
-		setTimeout(function() {
-		    console.log(index+1);
-		}, index * 1000);
-	    }(i));
-	}
+	countTo(10);
     }
 
     function countToTwenty() {
-	for(var i=0; i < 20; i++) {
+	countTo(20);
+    }
+
+
+    function countTo(x) {
+	for(var i=0; i < x; i++) {
 	    (function(index) {
 		setTimeout(function() {
 		    console.log(index+1);
 		}, index * 1000);
 	    }(i));
 	}
+
     }
-
-
+    
     this.countToTen    = countToTen;
     this.countToTwenty = countToTwenty;    
 
